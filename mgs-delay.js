5b11e08202b28ca8eda2b9b220bd94bdb850f0c6
Cyril <terry6394@gmail.com>
2021-02-28 20:08:50 +0800

    Update mgs-delay.js

--- mgs-delay.js
+++ mgs-delay.js
@@ -1,6 +1,7 @@
 console.log("========== 延迟脚本开始 ==========")
 let headers = $response.headers;
-headers['X-Modified-By'] = 'Surge';
+let operationType = headers['Operation-Type'];
+console.log("operationType: " + operationType);
 if(operationType.search('mainpageAuthFlag') > -1) {
   console.log(operationType.search('mainpageAuthFlag'));
   setTimeout(function(){alert("Hello world");},1000);
